21094d313007c2cfeec363db33fa5f860e0badaa
brilliswift <iambrillia@gmail.com>
2025-10-04 01:31:22 -0700

    refactor: use props destructuring for cleaner code

--- Paws-Connect/src/Components/AdopsiItem.jsx
+++ Paws-Connect/src/Components/AdopsiItem.jsx
@@ -1,15 +1,19 @@
+// NOTE: Consider wrapping this component with React.memo for performance optimization
+// if props (image, name, desc) are unlikely to change frequently.
+// Usage: import React, { memo } from "react"; export default memo(AdopsiItem);
 import React from "react";
 
-const AdopsiItem = (props) => {
+//refactor: use props destructuring for cleaner code
+const AdopsiItem = (image, name, desc) => {
   return (
     <>
       <div className="hover:cursor-pointer hover:scale-105 duration-300">
-        <img src={props.image} alt="adopsi" />
+        <img src={image} alt="adopsi" />
         <h2 className="pt-5 text-sky-950 text-[25px] font-bold font-Satoshi-Regular leading-[37.50px]">
-          {props.name}
+          {name}
         </h2>
         <p className="text-sky-950 text-xl font-light font-Satoshi-Light leading-[30px]">
-          {props.desc}
+          {desc}
         </p>
       </div>
     </>
